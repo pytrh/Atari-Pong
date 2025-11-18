b18981d04fcc9e603967885d139a682d81378ad7
Joseph Song <josephsong@V.local>
2025-11-18 03:49:10 +0100

    cmt

--- run_lander.py
+++ run_lander.py
@@ -3,8 +3,8 @@ import gymnasium as gym
 import numpy as np
 
 # from dqn_basic import DQNAgent
-from ddqn_basic import DQNAgent
-# from ddqn_uniform import DQNAgent
+# from ddqn_basic import DQNAgent
+from ddqn_uniform import DQNAgent
 
 import matplotlib.pyplot as plt
 import torch
